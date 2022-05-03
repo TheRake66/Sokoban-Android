7736e20117039f7ed042b0fd873e67a3d4713220
Thibault Bustos <thibault.bustos1234@gmail.com>
2022-05-03 09:03:37 +0200

    avancement

---
+++ app/src/main/java/com/example/sokoban/activity/MapSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.sokoban.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sokoban.R;
+
+public class MapSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_selection);
+    }
+}
