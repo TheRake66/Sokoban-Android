ce41f0661ccb3ce7b3424251f175859a08854897
Thibault Bustos <thibault.bustos1234@gmail.com>
2022-05-05 08:36:32 +0200

    map select

--- app/src/main/java/com/example/sokoban/activity/HomelActivity.java
+++ app/src/main/java/com/example/sokoban/activity/HomelActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.sokoban.MainActivity;
 import com.example.sokoban.R;
 
 public class HomelActivity extends AppCompatActivity {
